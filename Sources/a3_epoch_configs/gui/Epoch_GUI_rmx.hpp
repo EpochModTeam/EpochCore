7bba6d054638f45d4e2fe011ce5a9f9c325b095c
Raymix <raimonds.virtoss@gmail.com>
2016-06-18 02:21:18 +0100

    New Function: Epoch_message

--- Sources/a3_epoch_configs/gui/Epoch_GUI_rmx.hpp
+++ Sources/a3_epoch_configs/gui/Epoch_GUI_rmx.hpp
@@ -795,3 +795,24 @@ class rmx_ST2: RscStructuredText
 		shadow=2;
 	};
 };
+
+class rmx_t1
+{
+	text="";
+	x=0;
+	y=0;
+	h=0;
+	w=0;
+	type=0;
+	style=0x02;
+	shadow=2;
+	colorShadow[]={0,0,0,0.5};
+	font="PolenticalNeon";
+	SizeEx=0.05;
+	colorText[]={1,1,1,1};
+	colorBackground[]={0,0,0,0};
+	linespacing=1;
+	tooltipColorText[]={1,1,1,1};
+	tooltipColorBox[]={1,1,1,1};
+	tooltipColorShade[]={0,0,0,0.64999998};
+};
