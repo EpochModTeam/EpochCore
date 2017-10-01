c832121fdf0dc57b16c07343ab3542ad358d42ad
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2017-10-01 09:00:16 -0500

    Batch 2 most markers

--- Sources/a3_epoch_configs/Configs/CfgMarkers.hpp
+++ Sources/a3_epoch_configs/Configs/CfgMarkers.hpp
@@ -25,7 +25,579 @@ class cfgMarkers
 	class EpochSkull
 	{
 		name = "$STR_EPOCH_MarkerSkull";
-		icon = "\x\addons\a3_epoch_community\markers\marker_icon_skull.paa";
+		icon = "\x\addons\a3_epoch_community\markers\markericon_skull.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochAirdrop
+	{
+		name = "$STR_EPOCH_MarkerAirdrop";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_airdrop.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochAmmo
+	{
+		name = "$STR_EPOCH_MarkerAmmo";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_ammo.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochAttachment
+	{
+		name = "$STR_EPOCH_MarkerAttachment";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_attachment.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochBackpack
+	{
+		name = "$STR_EPOCH_MarkerBackpack";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_backpack.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochBiohazard
+	{
+		name = "$STR_EPOCH_MarkerBiohazard";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_biohazard.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochBoat
+	{
+		name = "$STR_EPOCH_MarkerBoat";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_boat.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochBricks
+	{
+		name = "$STR_EPOCH_MarkerBricks";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_bricks.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochCar
+	{
+		name = "$STR_EPOCH_MarkerCar";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_car.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochCarnival
+	{
+		name = "$STR_EPOCH_MarkerCarnival";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_carnival.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochCash
+	{
+		name = "$STR_EPOCH_MarkerCash";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_cash.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochChat
+	{
+		name = "$STR_EPOCH_MarkerChat";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_chat.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochContainer
+	{
+		name = "$STR_EPOCH_MarkerContainer";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_container.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochDollar1
+	{
+		name = "$STR_EPOCH_MarkerDollar1";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_dollar_1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochDollar2
+	{
+		name = "$STR_EPOCH_MarkerDollar2";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_dollar_2.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochWater
+	{
+		name = "$STR_EPOCH_MarkerWater";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_drinking_water.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochDynamicTrader
+	{
+		name = "$STR_EPOCH_MarkerDynamicTrader";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_dynamic_trader.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochExplosive
+	{
+		name = "$STR_EPOCH_MarkerExplosive";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_explosive.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochFirstAid
+	{
+		name = "$STR_EPOCH_MarkerFirstAid";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_first_aid.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochFood
+	{
+		name = "$STR_EPOCH_MarkerFood";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_food.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochGem
+	{
+		name = "$STR_EPOCH_MarkerGem";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_gem.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochHeart
+	{
+		name = "$STR_EPOCH_MarkerHeart";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_heart.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochHeli1
+	{
+		name = "$STR_EPOCH_MarkerHeli";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_heli_1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochHeli2
+	{
+		name = "$STR_EPOCH_MarkerHeli";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_heli_2.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochHelmet
+	{
+		name = "$STR_EPOCH_MarkerHelmet";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_helmet.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochJet
+	{
+		name = "$STR_EPOCH_MarkerJet";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_jet.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochKrypto
+	{
+		name = "$STR_EPOCH_MarkerKrypto";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_krypto.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochKryptoBag
+	{
+		name = "$STR_EPOCH_MarkerKryptoBag";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_krypto_cash.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochLightning
+	{
+		name = "$STR_EPOCH_MarkerLightning";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lightning.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionNoob1
+	{
+		name = "$STR_EPOCH_MarkerMission1_1";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_1_1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionNoob2
+	{
+		name = "$STR_EPOCH_MarkerMission1_2";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_1_2.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionMedium1
+	{
+		name = "$STR_EPOCH_MarkerMission2_1";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_2_1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionMedium2
+	{
+		name = "$STR_EPOCH_MarkerMission2_2";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_2_2.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionHard1
+	{
+		name = "$STR_EPOCH_MarkerMission3_1";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_3_1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionHard2
+	{
+		name = "$STR_EPOCH_MarkerMission3_2";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_3_2.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionVeteran1
+	{
+		name = "$STR_EPOCH_MarkerMission4_1";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_4_1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMissionVeteran2
+	{
+		name = "$STR_EPOCH_MarkerMission4_2";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_lvl_4_2.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochMixer
+	{
+		name = "$STR_EPOCH_MarkerMixer";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_mixer.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochPistol
+	{
+		name = "$STR_EPOCH_MarkerPistol";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_pistol.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochPlant
+	{
+		name = "$STR_EPOCH_MarkerPlant";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_plant.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochQuad
+	{
+		name = "$STR_EPOCH_MarkerQuad";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_quad.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochRadiation
+	{
+		name = "$STR_EPOCH_MarkerRadiation";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_radiation.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochRespawn
+	{
+		name = "$STR_EPOCH_MarkerRespawn";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_respawn.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochRifle
+	{
+		name = "$STR_EPOCH_MarkerRifle";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_rifle.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochService
+	{
+		name = "$STR_EPOCH_MarkerService";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_service.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochShipwreck
+	{
+		name = "$STR_EPOCH_MarkerShipwreck";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_shipwreck.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochSniper
+	{
+		name = "$STR_EPOCH_MarkerSniper";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_sniper.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochStaticTrader
+	{
+		name = "$STR_EPOCH_MarkerStaticTrader";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_static_trader.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochTank
+	{
+		name = "$STR_EPOCH_MarkerTank";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_tank.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochTruck
+	{
+		name = "$STR_EPOCH_MarkerTruck";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_truck.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochUniform
+	{
+		name = "$STR_EPOCH_MarkerUniform";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_uniform.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochVest
+	{
+		name = "$STR_EPOCH_MarkerVest";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_vest.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochWood
+	{
+		name = "$STR_EPOCH_MarkerWood";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_wood.paa";
 		color[] = {1,1,1,1};
 		size = 32;
 		scope = 2;
