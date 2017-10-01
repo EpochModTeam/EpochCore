eb4ab7a52e6ccc9bbe22e96dd43b2ccc11a97662
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2017-09-30 21:24:50 -0500

    DrokZ death marker and dyna menu icon + base marker assets active and hex
    Vanilla Hex and Active for all custom markers
    Death marker and Dyna Menu Icon.
    New hpp files added to Configs folder
    Includes added to config.cpp

---
+++ Sources/a3_epoch_configs/Configs/CfgMarkers.hpp
@@ -0,0 +1,36 @@
+class cfgMarkers
+{
+	class EpochActive
+	{
+		name = "$STR_EPOCH_MarkerActive";
+		icon = "\x\addons\a3_epoch_community\markers\marker_active.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochHex
+	{
+		name = "$STR_EPOCH_MarkerHex";
+		icon = "\x\addons\a3_epoch_community\markers\marker_hex.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+	class EpochSkull
+	{
+		name = "$STR_EPOCH_MarkerSkull";
+		icon = "\x\addons\a3_epoch_community\markers\marker_icon_skull.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		scope = 2;
+		scopeCurator = 2;
+		shadow = 1;
+		markerClass = "epochmod_assets";
+	};
+};
