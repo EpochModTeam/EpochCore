385aeef15e2b493a8f3e40d8a4da1bf5112b78b4
He-Man <Nullimeyer@yahoo.de>
2019-01-25 19:33:40 +0100

    E-Pad positions

--- Sources/a3_epoch_configs/gui/EPOCH_EPad.hpp
+++ Sources/a3_epoch_configs/gui/EPOCH_EPad.hpp
@@ -2,10 +2,27 @@ class RscButton_EPad : RscButton
 {
 	colorFocused[] = {0,0,0,0.5};
 	colorBackgroundActive[] = {0,0,0,0.5};
-	w = 0.0400931 * safezoneW;                                             
-	h = 0.0550151 * safezoneH;
+	w = 0.04 * safezoneW;                                             
+	h = 0.06 * safezoneH;
 	action = "";
 };
+class EPad_Button_Backgr : RscPicture
+{
+	text = "";
+	w = 0.04 * safezoneW;
+	h = 0.06 * safezoneH;
+};
+class EPad_Button_Txt : RscText
+{
+	style = 0x02;
+	w = 0.04 * safezoneW;
+	h = 0.016 * safezoneH;
+	colorBackground[] = {0,0,0,0.5};
+	colorText[] = {1,1,1,1};
+	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
+	text = "";
+};
+
 class RscButtonPDATrans : RscButton
 {
 	fade = 0;
@@ -31,10 +48,10 @@ class epoch_tablet_gui
 		{
 			idc = 1200;
 			text = "x\addons\a3_epoch_code\Data\UI\epad\epoch_tablet_back.paa";
-			x = 0.184983 * safezoneW + safezoneX;
-			y = 0.114894 * safezoneH + safezoneY;
-			w = 0.635763 * safezoneW;
-			h = 0.80322 * safezoneH;
+			x = 0.185 * safezoneW + safezoneX;
+			y = 0.115 * safezoneH + safezoneY;
+			w = 0.636 * safezoneW;
+			h = 0.84 * safezoneH;
 		};
 		class Spacer: RscButtonPDATrans
 		{
@@ -49,10 +66,10 @@ class epoch_tablet_gui
 		class close: RscButtonPDATrans
 		{
 			idc = 1600;
-			x = 0.574459 * safezoneW + safezoneX;
-			y = 0.213921 * safezoneH + safezoneY;
-			w = 0.0286379 * safezoneW;
-			h = 0.0440121 * safezoneH;
+			x = 0.575 * safezoneW + safezoneX;
+			y = 0.214 * safezoneH + safezoneY;
+			w = 0.029 * safezoneW;
+			h = 0.044 * safezoneH;
 			action = "closeDialog 0;";
 			tooltip = "Close PDA";
 		};
@@ -62,27 +79,17 @@ class epoch_tablet_gui
 			x = 0.416 * safezoneW + safezoneX;                                  
 			y = 0.295 * safezoneH + safezoneY;                                  
 		};
-		class app1_background: RscPicture
+		class app1_background: EPad_Button_Backgr
 		{
 			idc = 1701;
-			text = "";
 			x = 0.416 * safezoneW + safezoneX;
 			y = 0.295 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
 		};
-		class app1txt: RscText
+		class app1txt: EPad_Button_Txt
 		{
 			idc = 1801;
-			style = 0x02;
 			x = 0.416 * safezoneW + safezoneX;
-			y = (0.295+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.295+0.06) * safezoneH + safezoneY;
 		};
 		class app2: RscButton_EPad
 		{
@@ -90,27 +97,17 @@ class epoch_tablet_gui
 			x = 0.473 * safezoneW + safezoneX;
 			y = 0.295 * safezoneH + safezoneY;
 		};
-		class app2_background: RscPicture
+		class app2_background: EPad_Button_Backgr
 		{
 			idc = 1702;
-			text = "";
 			x = 0.473 * safezoneW + safezoneX;
 			y = 0.295 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
 		};
-		class app2txt: RscText
+		class app2txt: EPad_Button_Txt
 		{
 			idc = 1802;
-			style = 0x02;
 			x = 0.473 * safezoneW + safezoneX;
-			y = (0.295+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.295+0.06) * safezoneH + safezoneY;
 		};
 		class app3: RscButton_EPad
 		{
@@ -118,377 +115,263 @@ class epoch_tablet_gui
 			x = 0.53 * safezoneW + safezoneX;
 			y = 0.295 * safezoneH + safezoneY;
 		};
-		class app3_background: RscPicture
+		class app3_background: EPad_Button_Backgr
 		{
 			idc = 1703;
-			text = "";
 			x = 0.53 * safezoneW + safezoneX;
 			y = 0.295 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
 		};
-		class app3txt: RscText
+		class app3txt: EPad_Button_Txt
 		{
 			idc = 1803;
-			style = 0x02;
 			x = 0.53 * safezoneW + safezoneX;
-			y = (0.295+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.295+0.06) * safezoneH + safezoneY;
 		};
 		class app4: RscButton_EPad
 		{
 			idc = 1604;
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.372 * safezoneH + safezoneY;
+			y = 0.377 * safezoneH + safezoneY;
 		};
-		class app4_background: RscPicture
+		class app4_background: EPad_Button_Backgr
 		{
 			idc = 1704;
-			text = "";
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.372 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.377 * safezoneH + safezoneY;
 		};
-		class app4txt: RscText
+		class app4txt: EPad_Button_Txt
 		{
 			idc = 1804;
-			style = 0x02;
 			x = 0.416 * safezoneW + safezoneX;
-			y = (0.372+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.377+0.06) * safezoneH + safezoneY;
 		};
 		class app5: RscButton_EPad
 		{
 			idc = 1605;
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.372 * safezoneH + safezoneY;
+			y = 0.377 * safezoneH + safezoneY;
 		};
-		class app5_background: RscPicture
+		class app5_background: EPad_Button_Backgr
 		{
 			idc = 1705;
-			text = "";                                                             
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.372 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.377 * safezoneH + safezoneY;
 		};
-		class app5txt: RscText
+		class app5txt: EPad_Button_Txt
 		{
 			idc = 1805;
-			style = 0x02;
 			x = 0.473 * safezoneW + safezoneX;
-			y = (0.372+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.377+0.06) * safezoneH + safezoneY;
 		};
 		class app6: RscButton_EPad
 		{
 			idc = 1606;
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.372 * safezoneH + safezoneY;
+			y = 0.377 * safezoneH + safezoneY;
 		};
-		class app6_background: RscPicture
+		class app6_background: EPad_Button_Backgr
 		{
 			idc = 1706;
-			text = "";                                                             
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.372 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.377 * safezoneH + safezoneY;
 		};
-		class app6txt: RscText
+		class app6txt: EPad_Button_Txt
 		{
 			idc = 1806;
-			style = 0x02;
 			x = 0.53 * safezoneW + safezoneX;
-			y = (0.372+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.377+0.06) * safezoneH + safezoneY;
 		};
 		class app7: RscButton_EPad
 		{
 			idc = 1607;
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.449 * safezoneH + safezoneY;
+			y = 0.495 * safezoneH + safezoneY;
 		};
-		class app7_background: RscPicture
+		class app7_background: EPad_Button_Backgr
 		{
 			idc = 1707;
-			text = "";                                                             
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.449 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.495 * safezoneH + safezoneY;
 		};
-		class app7txt: RscText
+		class app7txt: EPad_Button_Txt
 		{
 			idc = 1807;
-			style = 0x02;
 			x = 0.416 * safezoneW + safezoneX;
-			y = (0.449+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.495+0.06) * safezoneH + safezoneY;
 		};
 		class app8: RscButton_EPad
 		{
 			idc = 1608;
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.449 * safezoneH + safezoneY;
+			y = 0.495 * safezoneH + safezoneY;
 		};
-		class app8_background: RscPicture
+		class app8_background: EPad_Button_Backgr
 		{
 			idc = 1708;
-			text = "";                                                             
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.449 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.495 * safezoneH + safezoneY;
 		};
-		class app8txt: RscText
+		class app8txt: EPad_Button_Txt
 		{
 			idc = 1808;
-			style = 0x02;
 			x = 0.473 * safezoneW + safezoneX;
-			y = (0.449+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.495+0.06) * safezoneH + safezoneY;
 		};
 		class app9: RscButton_EPad
 		{
 			idc = 1609;
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.449 * safezoneH + safezoneY;
+			y = 0.495 * safezoneH + safezoneY;
 		};
-		class app9_background: RscPicture
+		class app9_background: EPad_Button_Backgr
 		{
 			idc = 1709;
-			text = "";                                                             
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.449 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.495 * safezoneH + safezoneY;
 		};
-		class app9txt: RscText
+		class app9txt: EPad_Button_Txt
 		{
 			idc = 1809;
-			style = 0x02;
 			x = 0.53 * safezoneW + safezoneX;
-			y = (0.449+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.495+0.06) * safezoneH + safezoneY;
 		};
 		class app10: RscButton_EPad
 		{
 			idc = 1610;
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.526 * safezoneH + safezoneY;
+			y = 0.541 * safezoneH + safezoneY;
 		};
-		class app10_background: RscPicture
+		class app10_background: EPad_Button_Backgr
 		{
 			idc = 1710;
-			text = "";                                                             
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.526 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.541 * safezoneH + safezoneY;
 		};
-		class app10txt: RscText
+		class app10txt: EPad_Button_Txt
 		{
 			idc = 1810;
-			style = 0x02;
 			x = 0.416 * safezoneW + safezoneX;
-			y = (0.526+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.541+0.06) * safezoneH + safezoneY;
 		};
 		class app11: RscButton_EPad
 		{
 			idc = 1611;
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.526 * safezoneH + safezoneY;
+			y = 0.541 * safezoneH + safezoneY;
 		};
-		class app11_background: RscPicture
+		class app11_background: EPad_Button_Backgr
 		{
 			idc = 1711;
-			text = "";                                                             
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.526 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.541 * safezoneH + safezoneY;
 		};
-		class app11txt: RscText
+		class app11txt: EPad_Button_Txt
 		{
 			idc = 1811;
-			style = 0x02;
 			x = 0.473 * safezoneW + safezoneX;
-			y = (0.526+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.541+0.06) * safezoneH + safezoneY;
 		};
 		class app12: RscButton_EPad
 		{
 			idc = 1612;
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.526 * safezoneH + safezoneY;
+			y = 0.541 * safezoneH + safezoneY;
 		};
-		class app12_background: RscPicture
+		class app12_background: EPad_Button_Backgr
 		{
 			idc = 1712;
-			text = "";                                                             
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.526 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.541 * safezoneH + safezoneY;
 		};
-		class app12txt: RscText
+		class app12txt: EPad_Button_Txt
 		{
 			idc = 1812;
-			style = 0x02;
 			x = 0.53 * safezoneW + safezoneX;
-			y = (0.526+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.541+0.06) * safezoneH + safezoneY;
 		};
 		class app13: RscButton_EPad	// prev page
 		{
 			idc = 1614;
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.603 * safezoneH + safezoneY;
+			y = 0.623 * safezoneH + safezoneY;
 		};
-		class app13_background: RscPicture
+		class app13_background: EPad_Button_Backgr
 		{
 			idc = 1714;
 			text = "x\addons\a3_epoch_code\Data\UI\epad\back_ca.paa";
 			tooltip = "prev page";                                                             
 			x = 0.416 * safezoneW + safezoneX;
-			y = 0.603 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.623 * safezoneH + safezoneY;
 		};
 		class app14: RscButton_EPad
 		{
 			idc = 1613;
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.603 * safezoneH + safezoneY;
+			y = 0.623 * safezoneH + safezoneY;
 		};
-		class app14_background: RscPicture
+		class app14_background: EPad_Button_Backgr
 		{
 			idc = 1713;
-			text = "";                                                             
 			x = 0.473 * safezoneW + safezoneX;
-			y = 0.603 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.623 * safezoneH + safezoneY;
 		};
-		class app14txt: RscText
+		class app14txt: EPad_Button_Txt
 		{
 			idc = 1814;
-			style = 0x02;
 			x = 0.473 * safezoneW + safezoneX;
-			y = (0.603+0.0550151) * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.016 * safezoneH;
-			colorBackground[] = {0,0,0,0.5};
-			colorText[] = {1,1,1,1};
-			sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
-			text = "";
+			y = (0.623+0.06) * safezoneH + safezoneY;
 		};
 		class app15: RscButton_EPad  //next page
 		{
 			idc = 1615;
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.603 * safezoneH + safezoneY;
+			y = 0.623 * safezoneH + safezoneY;
 		};
-		class app15_background: RscPicture
+		class app15_background: EPad_Button_Backgr
 		{
 			idc = 1715;
 			text = "x\addons\a3_epoch_code\Data\UI\epad\next.paa";
 			tooltip = "next page";                                                             
 			x = 0.53 * safezoneW + safezoneX;
-			y = 0.603 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0550151 * safezoneH;
+			y = 0.623 * safezoneH + safezoneY;
 		};
 		class home: RscButtonPDATrans
 		{
 			idc = 1616;
-			x = 0.408359 * safezoneW + safezoneX;
-			y = 0.72006 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0440121 * safezoneH;
+			x = 0.408 * safezoneW + safezoneX;
+			y = 0.74 * safezoneH + safezoneY;
+			w = 0.04 * safezoneW;
+			h = 0.044 * safezoneH;
 			action = "Epoch_E_Pad_Page = 1; [] call EPOCH_EPad_dialog";
 			tooltip = "home";	
 		};
 		class call: RscButtonPDATrans
 		{
 			idc = 1617;
-			x = 0.454179 * safezoneW + safezoneX;
-			y = 0.72006 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0440121 * safezoneH;
+			x = 0.45 * safezoneW + safezoneX;
+			y = 0.74 * safezoneH + safezoneY;
+			w = 0.04 * safezoneW;
+			h = 0.044 * safezoneH;
 			action = "";
 			tooltip = "";	
 		};
 		class end: RscButtonPDATrans
 		{
 			idc = 1618;
-			x = 0.494272 * safezoneW + safezoneX;
-			y = 0.72006 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0440121 * safezoneH;
+			x = 0.5 * safezoneW + safezoneX;
+			y = 0.74 * safezoneH + safezoneY;
+			w = 0.04 * safezoneW;
+			h = 0.044 * safezoneH;
 			action = "";
 			tooltip = "";	
 		};
 		class sms: RscButtonPDATrans
 		{
 			idc = 1619;
-			x = 0.540093 * safezoneW + safezoneX;
-			y = 0.72006 * safezoneH + safezoneY;
-			w = 0.0400931 * safezoneW;
-			h = 0.0440121 * safezoneH;
+			x = 0.54 * safezoneW + safezoneX;
+			y = 0.74 * safezoneH + safezoneY;
+			w = 0.04 * safezoneW;
+			h = 0.044 * safezoneH;
 			action = "['WIP',5] call epoch_message";
 			tooltip = "sms (wip)";	
 		};
@@ -548,19 +431,19 @@ class epoch_tablet_gui
 		class PDASMSText_Box : RscStructuredText
 		{
 			idc = 1902;
-			x = 0.414086 * safezoneW + safezoneX;                                  
-			y = 0.301946 * safezoneH + safezoneY;                                  
-			w = 0.1646451 * safezoneW;
-			h = 0.3520961 * safezoneH;
+			x = 0.414 * safezoneW + safezoneX;                                  
+			y = 0.302 * safezoneH + safezoneY;                                  
+			w = 0.165 * safezoneW;
+			h = 0.372 * safezoneH;
 		};
 		class PDAListBox: RscListBox
 		{
 			idc = 1903;
 			onLBDblClick = "_this call Epoch_SMS_OpenDiag";
-			x = 0.414086 * safezoneW + safezoneX;                                  
-			y = 0.301946 * safezoneH + safezoneY;                                  
-			w = 0.1646451 * safezoneW;
-			h = 0.3520961 * safezoneH;
+			x = 0.414 * safezoneW + safezoneX;                                  
+			y = 0.302 * safezoneH + safezoneY;                                  
+			w = 0.165 * safezoneW;
+			h = 0.372 * safezoneH;
 		};
 	};
  };
@@ -574,10 +457,10 @@ class server_info_picture
        {
 			idc = 98999;
 			text = "x\addons\a3_epoch_code\Data\UI\epad\server_info_ca.paa";
-			x = 0.184983 * safezoneW + safezoneX;
-			y = 0.114894 * safezoneH + safezoneY;
-			w = 0.635763 * safezoneW;
-			h = 0.80322 * safezoneH;
+			x = 0.185 * safezoneW + safezoneX;
+			y = 0.115 * safezoneH + safezoneY;
+			w = 0.636 * safezoneW;
+			h = 0.8 * safezoneH;
        }; 
 	};	   
 };
