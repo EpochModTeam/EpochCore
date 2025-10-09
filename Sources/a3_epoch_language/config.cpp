2e37129611282e8575e088762e4f5b930edb81f1
vbawol <awol@veteranbastards.com>
2025-10-09 07:37:09 -0500

    remaining

---
+++ Sources/a3_epoch_language/config.cpp
@@ -0,0 +1,24 @@
+/*
+	Author: Aaron Clark - EpochMod.com
+
+    Contributors:
+
+	Description:
+	Epoch Language Config and stringtable.xml
+
+    Licence:
+    Arma Public License Share Alike (APL-SA) - https://www.bistudio.com/community/licenses/arma-public-license-share-alike
+
+    Github:
+    https://github.com/EpochModTeam/Epoch/tree/release/Sources/epoch_language/config.hpp
+*/
+class CfgPatches
+{
+	class A3_epoch_language
+	{
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		units[] = {};
+		epochVersion = "1.3.3";
+	};
+};
---
+++ Sources/a3_epoch_structures/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches
+{
+	class a3_epoch_structures
+	{
+		requiredAddons[] = {};
+		requiredVersion = 0.1;
+		epochVersion = "1.3.3";
+		units[] ={};
+		weapons[] = {};
+	};
+};
