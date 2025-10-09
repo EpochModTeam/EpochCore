e4eea6c65f7a7e4b92d94cac46b3162c012bf5d3
vbawol <awol@veteranbastards.com>
2025-10-09 07:24:07 -0500

    epoch_weapons

---
+++ Sources/a3_epoch_weapons/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches
+{
+	class a3_epoch_weapons
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+		epochVersion = "1.3.3";
+	};
+};
