244a6fcf9fa0c6a107e8c7f9bd261696714de754
vbawol <awol@veteranbastards.com>
2025-10-08 19:09:45 -0500

    Add a3_epoch_assets_2

---
+++ Sources/a3_epoch_assets_2/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches
+{
+	class epoch_objects
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+		epochVersion = "1.3.3";
+	};
+};
